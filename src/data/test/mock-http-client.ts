a5381194fe9e26ad6a8d71b30400bde1057697a1
Diego Oliveira de Araujo <doasalah@gmail.com>
2022-10-01 10:40:30 -0300

    test: ensure RemoteAuthentication calls HttpPostClient with correct body

--- src/data/test/mock-http-client.ts
+++ src/data/test/mock-http-client.ts
@@ -1,10 +1,13 @@
+import { AuthenticationParams } from '@/domain/usecases/authentication'
 import { HttpPostClient, HttpPostParams } from '../protocols/http/http-post-client'
 
 export class HttpPostClientSpy implements HttpPostClient {
   url?: string
+  body?: AuthenticationParams
 
   async post (params: HttpPostParams): Promise<void> {
     this.url = params.url
+    this.body = params.body
     // eslint-disable-next-line @typescript-eslint/return-await
     return Promise.resolve()
   }
